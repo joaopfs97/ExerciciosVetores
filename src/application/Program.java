c78bec9ab5d5c8e77c0bb9e9a0b29668a206b894
joaopfs97 <joaopfs201@gmail.com>
2022-08-08 16:10:52 -0300

    Exercicio1

---
+++ src/application/Program.java
@@ -0,0 +1,36 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+    
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        
+        int n = 0;
+        do{
+        System.out.println("Quantos números você vai digitar?");
+        n = sc.nextInt();
+        } while(n > 10);
+        
+        int [] vect = new int[n];
+        
+        for (int i = 0; i < vect.length; i++){
+            System.out.println ("Digite um numero: ");
+            vect[i] = sc.nextInt();
+        }
+        
+        System.out.println ("NUMERO NEGATIVOS:");
+    
+        for (int i = 0; i < vect.length; i++){
+            if(vect[i] < 0){
+                System.out.println (vect[i]);
+            }
+        }
+
+        sc.close();
+    }
+    
+}
