a5da124141971e1fd6e0c6546ee6e7a03232e615
joaopfs97 <joaopfs201@gmail.com>
2022-08-09 10:38:05 -0300

    Exercicio 7

--- src/application/Program.java
+++ src/application/Program.java
@@ -166,6 +166,30 @@ public class Program {
         }
         */
         
+        /* Exercicio 7
+        int n;
+        System.out.print("Quantos elementos vai ter o vetor? ");
+        n = sc.nextInt();
+        double[] vect = new double[n];
+        double sum = 0;
+        
+        for (int i = 0; i < vect.length; i++){
+            System.out.print("Digite um numero: ");
+            vect[i] = sc.nextDouble();
+            sum += vect[i];
+        }
+        
+        double avg = sum/vect.length;
+        System.out.println();
+        System.out.println("MEDIA DO VETOR = " + avg);
+        System.out.println("ELEMENTOS ABAIXO DA MEDIA:");
+        for (int i = 0; i < vect.length; i++){
+            if(vect[i] < avg){
+                System.out.println(vect[i]);
+            }
+        }
+        */
+        
         
         sc.close();
     }
