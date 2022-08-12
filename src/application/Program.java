b1a6d61227f3ca3cc7767dd975e259034555e6c7
joaopfs97 <joaopfs201@gmail.com>
2022-08-12 13:11:17 -0300

    Aprendendo matrizes

--- src/application/Program.java
+++ src/application/Program.java
@@ -460,6 +460,36 @@ public class Program {
         }
         */
         
+        // Exercicio Matriz
+        System.out.println("Qual será a dimensão da matrix?");
+        int n = sc.nextInt();
+        
+        int [][] mat = new int[n][n];
+        
+        for(int i = 0; i < n; i++){
+            for (int j = 0; j < n; j++){
+                mat[i][j] = sc.nextInt();
+            }
+        }
+        
+        System.out.println("Main diagonal:");
+        for(int i = 0; i < n; i++){
+            System.out.print(mat[i][i] + " ");
+        }
+        System.out.println();
+        
+        int count = 0;
+        
+        for(int i = 0; i < mat.length; i++){
+            for (int j = 0; j < mat[i].length; j++){
+                if(mat[i][j] < 0){
+                    count ++;
+                }
+            }
+        }
+        
+        System.out.println("Negative numbers = " + count);
+        
         sc.close();
     }
     
